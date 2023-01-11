2942647b025714f6983e5d586397bb3fd0da1ec4
Thiago Ferreira <62024096+thiagosouf@users.noreply.github.com>
2023-01-11 13:38:37 -0300

    classe endereço

---
+++ Endereco.java
@@ -0,0 +1,27 @@
+public class Endereco {
+    private String rua = "Rua dos Bobos";
+    private int numero = 0;
+    private String bairro = "Esmero";
+    private String cidade = "Rio de Janeiro";
+    private String estado = "RJ";
+
+    public String getRua() {
+        return this.rua;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public String getBairro() {
+        return this.bairro;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+}
